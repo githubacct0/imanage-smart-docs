01ed833a73a5074784f3b0c1286650765d79d7eb
Inaxion <dev@Inaxion.agency>
2021-08-26 21:49:56 -0400

    header updates

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -20,15 +20,21 @@
       >
         &lt; Back
       </router-link>
-      <h1 class="mt-2">PIntegra Drag and Drop Starter App</h1>
+      <h1 class="mt-2">Integra Drag and Drop Starter App</h1>
       <p class="mt-5">
         This is a simple node application developed by Integra to help other
         developers get a head start on processing Smart Documents. The
         application simply takes a Smart PDF Document and checks the hash of the
         document against the Integra Ledger Blockchain, if it is valid it will
         return the metadata contained within the document and return it to the
-        user. The repo can be found at
-        https://github.com/IntegraLedger/DragAndDropStarter.
+        user as a JSON object.<br />
+        The repo can be found at
+        <a
+          href="https://github.com/IntegraLedger/DragAndDropStarter"
+          target="_blank"
+          style="color:white"
+          ><i><u>https://github.com/IntegraLedger/DragAndDropStarter</u></i>
+        </a>
       </p>
     </v-col>
   </v-sheet>
