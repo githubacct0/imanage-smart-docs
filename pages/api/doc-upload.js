85e3709ffd61c3cca9110cab7be4f05297cbe692
brwist <allgoodworks79@gmail.com>
2021-09-03 21:14:33 +0200

    Removed unused server upload logic

--- pages/api/doc-upload.js
+++ pages/api/doc-upload.js
@@ -1,11 +1,3 @@
-import formidable from "formidable";
-
-export const config = {
-    api: {
-        bodyParser: false
-    }
-};
-
 export default function handler(req, res) {
     res.status(200).json({ name: 'John Doe' })
 }
