2900a048695bdea159080fa5e4a7866dc2d507e3
brwist <allgoodworks79@gmail.com>
2021-09-03 19:44:43 +0200

    Done with some of the UI, missing upload functionality

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
+import React from 'react'
 import Head from 'next/head'
-import Image from 'next/image'
+import React from 'react'
 import styles from '../styles/Home.module.css'
 
 const Logo = () => (
